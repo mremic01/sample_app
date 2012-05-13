df49bc042613d87af50f97c31504cea57ea589a7
Mike Remick <mremic01@gmail.com>
2012-05-13 11:50:38 -0500

    done with ch11

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,3 +1,4 @@
+# By using the symbol ':user', we get Factory Girl to simulate the User model.
 Factory.define :user do |user|
   user.name                  "Michael Hartl"
   user.email                 "mhartl@example.com"
@@ -5,10 +6,18 @@ Factory.define :user do |user|
   user.password_confirmation "foobar"
 end
 
-Factory.sequence :name do |n|
-  "Person #{n}"
-end
-
 Factory.sequence :email do |n|
   "person-#{n}@example.com"
+end
+
+
+#added this at the end of ch11 to get tests passing for get index in users controller
+Factory.sequence :name do |n|
+  "person-#{n}"
+end
+
+
+Factory.define :micropost do |micropost|
+  micropost.content "Foo bar"
+  micropost.association :user
 end
